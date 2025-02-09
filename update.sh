150b64f30e76459ac2433a4303832bad424e8995
陈波 <2207117672@qq.com>
2025-02-09 11:21:09 +0800

    更新update文件

--- update.sh
+++ update.sh
@@ -3,6 +3,9 @@
 # 日志文件路径
 LOG_FILE="update.log"
 
+#丢弃本地更改
+git reset --hard HEAD
+
 # 获取当前分支的最新提交哈希
 current_hash=$(git rev-parse HEAD)
 
@@ -14,15 +17,16 @@ remote_hash=$(git rev-parse origin/$(git branch --show-current))
 
 # 比较本地和远程的提交哈希
 if [ "$current_hash" != "$remote_hash" ]; then
-    echo "$(date): 代码已更新，正在同步远程代码到本地..." | tee -a "$LOG_FILE"
+    echo "$(date): 代码已更新，正在同步远程代码到本地..."
+#     | tee -a "$LOG_FILE"
     # 将远程代码合并到本地分支
-    git merge origin/$(git branch --show-current) | tee -a "$LOG_FILE"
-    echo "$(date): 远程代码已同步到本地。" | tee -a "$LOG_FILE"
+    git merge origin/$(git branch --show-current)
+    echo "$(date): 远程代码已同步到本地。"
 
     # 重启 Docker 容器
-    echo "$(date): 正在重启 Docker 容器..." | tee -a "$LOG_FILE"
-    docker restart mafl-mafl-1 | tee -a "$LOG_FILE"
-    echo "$(date): Docker 容器已重启。" | tee -a "$LOG_FILE"
+    echo "$(date): 正在重启 Docker 容器..."
+    docker restart mafl-mafl-1
+    echo "$(date): Docker 容器已重启。"
 else
-    echo "$(date): 代码未更新，无需同步或重启 Docker 容器。" | tee -a "$LOG_FILE"
+    echo "$(date): 代码未更新，无需同步或重启 Docker 容器。"
 fi
